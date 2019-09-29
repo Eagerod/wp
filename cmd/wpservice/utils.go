73df6f1cce51e395193563893d7514683fbe2d9c
Aleem Haji <hajial@gmail.com>
2019-09-29 15:52:00 -0400

    Added in some tests.

--- cmd/wpservice/utils.go
+++ cmd/wpservice/utils.go
@@ -17,22 +17,26 @@ import (
     "strings"
 )
 
+var dimensionsRegexp *regexp.Regexp = regexp.MustCompile(`^(\d+)x(\d+)$`)
+
 /*
   Parse a string in the form <x>x<y> and return a Point specifying the extents
 */
 func ParseDimensionsString(str string) (image.Point, error) {
-    dimensionsRegexp := regexp.MustCompile(`^(\d+)x(\d+)$`)
-
     dimensionsMatch := dimensionsRegexp.FindStringSubmatch(str)
 
+    if len(dimensionsMatch) == 0 {
+        return image.ZP, errors.New(fmt.Sprintf("Provided dimension string (%s) is not valid", str))
+    }
+
     width, err := strconv.Atoi(dimensionsMatch[1])
-    if err != nil {
-        return image.ZP, errors.New("Provided width is not a valid integer")
+    if err != nil || width <= 0 {
+        return image.ZP, errors.New("Provided width is not a valid positive integer")
     }
 
     height, err := strconv.Atoi(dimensionsMatch[2])
-    if err != nil {
-        return image.ZP, errors.New("Provided height is not a valid integer")
+    if err != nil || height <= 0 {
+        return image.ZP, errors.New("Provided height is not a valid positive integer")
     }
 
     return image.Pt(width, height), nil
--- cmd/wpservice/utils_test.go
+++ cmd/wpservice/utils_test.go
@@ -1,6 +1,7 @@
 package wpservice;
 
 import (
+    "image"
     "testing"
 )
 
@@ -14,4 +15,25 @@ func TestParseDimensionsStringValid(t *testing.T) {
     assert.NoError(t, err)
     assert.Equal(t, point.X, 1024)
     assert.Equal(t, point.Y, 768)
+}
+
+func TestParseDimensionsStringInvalid(t *testing.T) {
+    point, err := ParseDimensionsString("3D")
+
+    assert.Equal(t, point, image.ZP)
+    assert.Equal(t, err.Error(), "Provided dimension string (3D) is not valid")
+}
+
+func TestParseDimensionsStringInvalidWidth(t *testing.T) {
+    point, err := ParseDimensionsString("0x768")
+
+    assert.Equal(t, point, image.ZP)
+    assert.Equal(t, err.Error(), "Provided width is not a valid positive integer")
+}
+
+func TestParseDimensionsStringInvalidHeight(t *testing.T) {
+    point, err := ParseDimensionsString("1024x0")
+
+    assert.Equal(t, point, image.ZP)
+    assert.Equal(t, err.Error(), "Provided height is not a valid positive integer")
 }
