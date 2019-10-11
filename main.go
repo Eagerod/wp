a40db73ea05e5e0ba967d7d78cb7a6718256d78f
aleem <aleem@haji.ca>
2019-10-11 00:59:38 +0000

    Makes it so image dimension errors don't cause the CLI to fail. (#6)

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 // Need to load up the image libraries for them to be registered for decoding.
 // Yay side effects!
 import (
+	"fmt"
 	"os"
+	"regexp"
 )
 
 import (
@@ -14,18 +16,32 @@ import (
 	"gitea.internal.aleemhaji.com/aleem/wp/cmd/wpservice"
 )
 
+var softErrorRegexp *regexp.Regexp = regexp.MustCompile(`^(?:Image is not (?:tall|wide) enough to produce quality output\n?)+$`)
+
 func main() {
 	baseCommand := &cobra.Command{
 		Use:   "wp <DesiredDimensions> <DestinationDir> <ImagePath> [ImagePath...] ",
 		Short: "Wallpaper Generator CLI",
-		Long:  "Create many different  of an image passed in",
+		Long:  "Create many different slices of an image passed in",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			desiredDimensions := args[0]
 			destinationDir := args[1]
 			imagePath := args[2]
 
-			return wpservice.ExtractFromLocalImage(desiredDimensions, destinationDir, imagePath)
+			err := wpservice.ExtractFromLocalImage(desiredDimensions, destinationDir, imagePath)
+
+			// If the only thing the error is is a series of soft errors, don't
+			//   exit with failure.
+			if err != nil {
+				if softErrorRegexp.FindStringSubmatch(err.Error()) == nil {
+					return err
+				}
+
+				fmt.Fprintln(os.Stderr, err.Error())
+			}
+
+			return nil
 		},
 	}
 
