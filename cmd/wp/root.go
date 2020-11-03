626bbd20e402664884fd4898989b6ecda37f97fe
Aleem Haji <hajial@gmail.com>
2020-11-02 23:23:15 -0500

    Moves the pick command to its own file.

--- cmd/wp/root.go
+++ cmd/wp/root.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 	"regexp"
 
-	"github.com/Eagerod/wp/pkg/wp"
 	"github.com/spf13/cobra"
 )
 
@@ -19,47 +17,6 @@ var baseCommand = &cobra.Command{
 	Use:   os.Args[0],
 	Short: "Wallpaper Generator CLI",
 	Long:  "Manipulate images for use as desktop wallpapers",
-}
-
-var pickCommand = &cobra.Command{
-	Use:   "pick desired_dimensions destination_dir gravity [--scaled] image_path [image_path...]",
-	Short: "Pick a single image slice",
-	Long:  "Extract a single slice of an image with the given parameters",
-	Args:  cobra.MinimumNArgs(4),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		desiredDimensions := args[0]
-		destinationDir := args[1]
-		gravity := args[2]
-		imagePaths := args[3:]
-
-		var errs []error
-		for _, imagePath := range imagePaths {
-			is, err := wp.PrepareImageFromSource(imagePath, cacheDir)
-			if err != nil {
-				return err
-			}
-			defer wp.CleanupImageSource(is)
-
-			err = wp.PickFromImage(desiredDimensions, destinationDir, is, scaledFlag, gravity)
-
-			if err != nil {
-				errs = append(errs, err)
-			}
-		}
-
-		// If the only thing the error is is a series of soft errors, don't
-		//   exit with failure.
-		multiError := wp.MultiErrorFromErrors(errs)
-		if multiError.Exists() {
-			if softErrorRegexp.FindStringSubmatch(multiError.Error()) == nil {
-				return multiError
-			}
-
-			fmt.Fprintln(os.Stderr, multiError.Error())
-		}
-
-		return nil
-	},
 }
 
 func Execute() {
