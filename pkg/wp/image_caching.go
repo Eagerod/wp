25a85c6a4d2003bec2b3a2ed2bca00f0d75129b1
Aleem Haji <hajial@gmail.com>
2021-04-20 22:14:59 -0400

    Prevent "caching" files that already live in the cache directory (#5)
    * Add in a piece to prevent duplicating files in the cache dir.
    
    * Updates cache fix to use absolute paths.
    
    * Adds warning about symlinks.

--- pkg/wp/image_caching.go
+++ pkg/wp/image_caching.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 )
 
 type ImageSource struct {
@@ -15,6 +16,15 @@ type ImageSource struct {
 	LocalPath  string
 
 	deleteParentDir bool
+}
+
+func (is *ImageSource) SourcePathIsLocal() (bool, error) {
+	pathUrl, err := url.Parse(is.SourcePath)
+	if err != nil {
+		return false, err
+	}
+
+	return pathUrl.Scheme == "" || pathUrl.Scheme == "file", nil
 }
 
 // Get the local image path from a given source image.
@@ -58,6 +68,39 @@ var downloadFile FileDownloader = func(destFile, sourceUrl string) error {
 func PrepareImageFromSource(sourcePath string, cacheDir string) (*ImageSource, error) {
 	is := ImageSource{}
 	is.SourcePath = sourcePath
+
+	// If the SourcePath is rooted in the cache directory, bail early, because
+	//   there's nothing interesting to do, and trying to use any kind of
+	//   recaching logic will just duplicate the image in the cache directory.
+	// Doesn't obey obey symlinks, for now.
+	if cacheDir != "" {
+		isLocal, err := is.SourcePathIsLocal()
+		if err != nil {
+			return nil, err
+		}
+
+		if isLocal {
+			absSource, err := filepath.Abs(is.SourcePath)
+			if err != nil {
+				return nil, err
+			}
+
+			absCache, err := filepath.Abs(cacheDir)
+			if err != nil {
+				return nil, err
+			}
+
+			if strings.HasPrefix(absSource, absCache) {
+				if _, err := os.Stat(is.SourcePath); err != nil {
+					return nil, err
+				}
+
+				is.LocalPath = is.SourcePath
+				is.deleteParentDir = false
+				return &is, nil
+			}
+		}
+	}
 
 	var outputDir string
 	if cacheDir == "" {
